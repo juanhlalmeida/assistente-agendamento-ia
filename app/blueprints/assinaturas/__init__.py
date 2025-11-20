f7aca6a631591832d159e08ab8a268a68b92b44d
juanhlalmeida <65877395+juanhlalmeida@users.noreply.github.com>
2025-11-20 16:27:33 -0300

    Initialize assinaturas blueprint in Flask app

--- app/blueprints/assinaturas/__init__.py
+++ app/blueprints/assinaturas/__init__.py
@@ -1,1 +1,8 @@
 
+# app/blueprints/assinaturas/__init__.py
+
+from flask import Blueprint
+
+bp = Blueprint('assinaturas', __name__, url_prefix='/assinatura')
+
+from app.blueprints.assinaturas import routes
