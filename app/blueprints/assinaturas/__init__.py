fb6675d8b5ad52f0596692791ae34b05b539e03c
juanhlalmeida <65877395+juanhlalmeida@users.noreply.github.com>
2025-11-22 15:30:02 -0300

    Enhance 'assinaturas' blueprint with template folder
    Added a template folder to the 'assinaturas' blueprint.

--- app/blueprints/assinaturas/__init__.py
+++ app/blueprints/assinaturas/__init__.py
@@ -1,8 +1,15 @@
-
 # app/blueprints/assinaturas/__init__.py
-
+"""
+Blueprint de assinaturas e pagamentos
+"""
 from flask import Blueprint
 
-bp = Blueprint('assinaturas', __name__, url_prefix='/assinatura')
+bp = Blueprint(
+    'assinaturas',
+    __name__,
+    url_prefix='/assinatura',
+    template_folder='../../templates/assinatura'
+)
 
+# Importa as rotas depois de criar o blueprint
 from app.blueprints.assinaturas import routes
